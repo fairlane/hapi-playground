0f74fc253079e0b77c50cb9224e779cc60c14855
Hannu Pölönen <hannu.polonen@diktamen.com>
2022-01-15 13:14:14 +0200

    Remove unused imports

--- test/unit/passwordutil.test.js
+++ test/unit/passwordutil.test.js
@@ -3,11 +3,11 @@
 const Lab = require('@hapi/lab');
 const { encryptString } = require("../../dist/util/PasswordUtil");
 const { expect } = require('@hapi/code');
-const { afterEach, beforeEach, describe, it } = exports.lab = Lab.script();
+const { describe, it } = exports.lab = Lab.script();
 
 describe('Password is encrypted', () => {
   it('encrypts', async () => {
     const pass = encryptString('test-pass', 'test-salt');
-    expect(pass).to.equal('7e5ef15ae28693ae3881379a71c97b54dffc82171b5efc0813c51993d05ded59');
+    expect(pass).equal('7e5ef15ae28693ae3881379a71c97b54dffc82171b5efc0813c51993d05ded59');
   })
 });
