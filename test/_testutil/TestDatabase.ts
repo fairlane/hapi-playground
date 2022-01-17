617253ad25a31a8ecb4aa860d8e9878707de6d54
Hannu Pölönen <hannu.polonen@diktamen.com>
2022-01-17 22:19:07 +0200

    Fix test connection creation to check for existing connections

--- test/_testutil/TestDatabase.ts
+++ test/_testutil/TestDatabase.ts
@@ -1,15 +1,23 @@
-import {Connection, createConnection} from "typeorm";
+import {Connection, createConnection, getConnection} from "typeorm";
+import {rejects} from "assert";
 
 // TODO - should create a connection for test db
 const ENTITIES_DIR = __dirname + "/../../src/entity/*{.js,.ts}";
 const createTestConnection = (): Promise<Connection> => {
 
-  return createConnection({
-    type: 'postgres',
-    url: process.env.TEST_DATABASE_URL,
-    synchronize: true,
-    entities: [ENTITIES_DIR]
-  })
+  try {
+    const conn: Connection = getConnection();
+    return new Promise<Connection>(function(resolve, reject) {
+      resolve(conn);
+    });
+  } catch (ConnectionNotFoundError) {
+    return createConnection({
+      type: 'postgres',
+      url: process.env.TEST_DATABASE_URL,
+      synchronize: true,
+      entities: [ENTITIES_DIR]
+    });
+  }
 }
 
 export {createTestConnection};
